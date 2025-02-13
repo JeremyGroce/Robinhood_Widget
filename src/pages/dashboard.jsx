e73b74ada535dba9243746b1203f1f8a334fc4d6
Jeremy Groce <JJGroce95@gmail.com>
2025-02-12 20:09:14 -0600

    updates

---
+++ src/pages/dashboard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function dashboard() 
+{
+
+    return(
+        <div className="dashboard-page">
+            <h1>dashboard</h1>
+        </div>
+    )
+}
+
+export default dashboard;
---
+++ src/pages/login.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import '../styling/log.css';
+
+function Login ()
+{
+    return(
+        <div className="login-page">
+            <h1>login page</h1>
+        </div>
+    )
+}
+
+export default Login;
