5b3fa678ac318c2df17e858c3a5a832b308c20a5
Jeremy Groce <JJGroce95@gmail.com>
2025-02-20 11:30:06 -0600

    installed typescript

---
+++ backend/server.ts
@@ -0,0 +1,32 @@
+// dependencies
+const express = require('express');
+const axios = require('axios');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const PORT = 5000;
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+// // routes
+// app.post('/login', (req: Request, res: Response) =>
+// {
+//     console.log('|pong');
+//     const {username, password} = req.body;
+
+//     if (!username || !password)
+//     {
+//         return res.status(400).json({message: 'username & pass required'});
+//     }
+//     else
+//     {
+//         return res.status(200).json({message: 'Login Successful'});
+//     }
+// });
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
